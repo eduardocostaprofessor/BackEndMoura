9d51b41e3faafb6c263b7562270f96d1c57e30d8
Eduardo Costa <eduardo.costa@docente.senai.br>
2025-10-31 09:16:14 -0300

    Adiciona os projetos de POO

---
+++ POO/PrimeiraClasse/Pessoa.cs
@@ -0,0 +1,22 @@
+namespace PrimeiraClasse
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        public int Altura;//em centímetros
+
+        public void Falar()
+        {
+            Console.WriteLine($"Olá, eu sou {Nome} e tenho {Idade} anos!");
+        }
+        public void Dormir()
+        {
+            Console.WriteLine($"ZZZzzzz ...");
+        }
+// criar um objeto da classe Pessoa
+// preencher as propriedades da classe Pessoa
+// rodar os métodos da classe pessoa, parecido com o que fizemos na classe
+// Carro
+    }
+}
---
+++ POO/PrimeiraClasse/Program.cs
@@ -0,0 +1,23 @@
+using PrimeiraClasse;
+
+Console.WriteLine($"==== PROGRAMA PRIMEIRA CLASSE ====");
+
+// Carro mclaren = new Carro();
+// mclaren.marca = "Mclaren";
+// mclaren.cor = "Laranja";
+// mclaren.modelo = "750s";
+// mclaren.potencia = 800;
+
+// Console.WriteLine($"Ligando a {mclaren.marca} {mclaren.cor}");
+// mclaren.Ligar();
+// Console.WriteLine($"Acelerando a {mclaren.marca} de {mclaren.potencia} cv");
+// mclaren.Acelerar();
+
+
+Pessoa edu = new Pessoa();
+edu.Nome = "Eduardo Mendes";
+edu.Idade = 43;
+edu.Altura = 166;
+
+edu.Falar();
+edu.Dormir();
